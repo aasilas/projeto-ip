a77d954065316b0adb03525693741bd85be4e5a6
afs9 <afs9@cin.ufpe.br>
2013-03-25 17:49:15 +0000

    lista e array de funcionarios

---
+++  projeto-ip/Projeto IP/src/dados/funcionarios/repositorios/ArrayFuncionarios.java
@@ -0,0 +1,59 @@
+package dados.funcionarios.repositorios;
+
+import dados.pessoas.Funcionario;
+import Interfaces.IRepositorioFuncionario;
+
+public class ArrayFuncionarios implements IRepositorioFuncionario{
+
+	private Funcionario[] arrayFuncionarios;
+	
+	public ArrayFuncionarios (){
+		this.arrayFuncionarios = new Funcionario[0];
+	}
+	
+	@Override
+	public void inserirFuncionario(Funcionario funcionario) {
+		Funcionario[] auxArray = this.arrayFuncionarios;
+		this.arrayFuncionarios = new Funcionario[arrayFuncionarios.length +1];
+		for (int i = 0; i < auxArray.length; i++) {
+			this.arrayFuncionarios[i] = auxArray[i];
+		}
+		this.arrayFuncionarios[arrayFuncionarios.length - 1] = funcionario;
+	}
+
+	@Override
+	public void removerFuncionario(String cpf) {
+		Funcionario[] auxArray = new Funcionario[arrayFuncionarios.length -1];
+		for (int i = 0, j =0; i < arrayFuncionarios.length; i++) {
+			if(arrayFuncionarios[i].getCpf() != cpf){
+				auxArray[j] = arrayFuncionarios[i]; 
+				j++;
+			}
+		}
+		this.arrayFuncionarios = auxArray;
+	}
+
+	@Override
+	public void atualizar(Funcionario funcionario) {
+		for (int i = 0; i < this.arrayFuncionarios.length; i++) {
+			if(this.arrayFuncionarios[i].getCpf() == funcionario.getCpf()){
+				this.arrayFuncionarios[i] = funcionario;
+			}
+		}
+	}
+
+	@Override
+	public Funcionario pesquisarFuncionario(String cpf) {	
+		Funcionario tempFuncionario = null;
+		for (int i = 0; i < this.arrayFuncionarios.length; i++) {
+			if(this.arrayFuncionarios[i].getCpf().equals(cpf)){
+				tempFuncionario = this.arrayFuncionarios[i];
+			}
+			else{
+				//TODO exception
+			}
+		}
+		return tempFuncionario;
+	}
+
+}
---
+++  projeto-ip/Projeto IP/src/dados/funcionarios/repositorios/ListaFuncionarios.java
@@ -0,0 +1,79 @@
+package dados.funcionarios.repositorios;
+
+import dados.pessoas.Funcionario;
+import Interfaces.IRepositorioFuncionario;;
+
+public class ListaFuncionarios implements IRepositorioFuncionario{
+	
+	private Funcionario funcionario;
+	private ListaFuncionarios proximo;
+	
+	public ListaFuncionarios (Funcionario funcionario){
+		this.funcionario = funcionario;
+		this.proximo = null;
+	}
+	
+	@Override
+	public void inserirFuncionario(Funcionario funcionario) {
+		if(this.funcionario == null){
+			this.funcionario = funcionario;
+			this.proximo = new ListaFuncionarios(null);
+		}
+		else{
+			if(this.proximo == null){
+				this.proximo = new ListaFuncionarios(funcionario); 
+			}
+			else{
+				this.proximo.inserirFuncionario(funcionario);
+			}
+		}
+	}
+
+	@Override
+	public void removerFuncionario(String cpf) {
+		if (this.funcionario != null) {
+			if (this.funcionario.getCpf() == cpf) {
+				this.funcionario = this.proximo.funcionario;
+				this.proximo = this.proximo.proximo;
+			} else{
+				this.proximo.removerFuncionario(cpf);
+			} 
+		} else {
+			// TODO exception
+		}
+	}
+
+	@Override
+	public void atualizar(Funcionario funcionario) {
+		if(this.funcionario != null){
+			if(this.funcionario.getCpf() == funcionario.getCpf()){
+				this.funcionario = funcionario;
+			}
+			else{
+				this.proximo.atualizar(funcionario);
+			}
+		}
+		else{
+			//TODO exception
+		}
+	}
+
+	@Override
+	public Funcionario pesquisarFuncionario(String cpf) {	
+		Funcionario funcionarioProcurado = null;
+		if(this.funcionario != null){
+			if(this.funcionario.getCpf() == cpf){
+				funcionarioProcurado = this.funcionario;
+			}
+			else{
+				funcionarioProcurado = this.proximo.pesquisarFuncionario(cpf);
+			}
+		}
+		else{
+			//TODO exception
+		}
+		return funcionarioProcurado;
+	}
+
+
+}
