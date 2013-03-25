10a7a9e6196fd9b45da64394f29bd4f3c0fa78b6
afs9@cin.ufpe.br <afs9@cin.ufpe.br@8858dfbd-2ff1-90cf-2671-067a58088875>
2013-03-25 17:49:15 +0000

    lista e array de funcionarios

--- trunk/ projeto-ip/Projeto IP/src/Interfaces/IRepositorioFuncionario.java
+++ trunk/ projeto-ip/Projeto IP/src/Interfaces/IRepositorioFuncionario.java
@@ -6,10 +6,10 @@ public interface IRepositorioFuncionario {
 
 	public void inserirFuncionario(Funcionario funcionario);
 	
-	public void removerFuncionario(Funcionario funcionario);
+	public void removerFuncionario(String cpf);
 	
-	public void atualizar();
+	public void atualizar(Funcionario funcionario);
 	
-	public Funcionario pesquisarFuncionario();
+	public Funcionario pesquisarFuncionario(String cpf);
 	//sem parametro pq ainda não definimos o tipo d busca que acontecerá.
 }
