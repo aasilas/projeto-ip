bba8987e2f0ddd71f54b5f083597c9f1b488b1d6
afs9@cin.ufpe.br <afs9@cin.ufpe.br@8858dfbd-2ff1-90cf-2671-067a58088875>
2013-03-25 05:22:10 +0000

    arrayCarros

--- trunk/ projeto-ip/Projeto IP/src/Repositorios/Teste.java
+++ trunk/ projeto-ip/Projeto IP/src/Repositorios/Teste.java
@@ -1,7 +1,7 @@
 package Repositorios;
 
 import dados.carros.*;
-import dados.carros.repositorios.ListaCarros;
+import dados.carros.repositorios.*;
 public class Teste {
 
 	
@@ -10,13 +10,16 @@ public class Teste {
 		Adicionais ad = new Adicionais(true, true, true, true, true, true, true); 
 		Carro car= new Carro(5, "1.6","fox", "vw", "f", ad, 540.00);
 		Carro car2= new Carro(5, "1.0","gol", "vw", "f", ad, 540.00);
-		ListaCarros carros = new ListaCarros(car);
+		Carro car3 = new Carro(5, "1.0","polo", "vw", "fdff", ad, 540.00);
+		ArrayCarros carros = new ArrayCarros();
+		carros.inserirCarro(car);
 		carros.inserirCarro(car2);
+		carros.inserirCarro(car3);
 		carros.removerCarro("fox");
 		car2 = new Carro(5, "1.0","gol", "vw", "fdff", ad, 540.00);
 		carros.atualizar(car2);
 		carros.inserirCarro(car);
-		Carro car3 = carros.pesquisarCarro("fox");
+		Carro car4 = carros.pesquisarCarro("fox");
 	}
 
 }
